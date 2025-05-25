6e26c3b9d9f62684262f401ed49e7657f82c796f
nicholls73 <jamnich73@gmail.com>
2025-05-25 12:33:17 +0800

    Add config fields

--- src/config/config.go
+++ src/config/config.go
@@ -2,17 +2,30 @@ package config
 
 import (
 	"os"
+	"time"
 
 	"gopkg.in/yaml.v3"
 )
 
 type Config struct {
-	IneligiblePatterns []string `yaml:"ineligible_patterns"`
+	IneligiblePatterns     []string  `yaml:"ineligible_patterns"`
+	SpendAmountForBonus    float64   `yaml:"spend_amount_for_bonus"`
+	BonusSpendPeriodInDays int       `yaml:"bonus_spend_period_in_days"`
+	CardStartDate          time.Time `yaml:"card_start_date"`
 }
 
 func (c *Config) Equals(other *Config) bool {
 	if c == nil || other == nil {
 		return c == other
+	}
+	if c.SpendAmountForBonus != other.SpendAmountForBonus {
+		return false
+	}
+	if c.BonusSpendPeriodInDays != other.BonusSpendPeriodInDays {
+		return false
+	}
+	if !c.CardStartDate.Equal(other.CardStartDate) {
+		return false
 	}
 	if len(c.IneligiblePatterns) != len(other.IneligiblePatterns) {
 		return false
